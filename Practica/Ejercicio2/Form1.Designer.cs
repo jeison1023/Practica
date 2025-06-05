5ce3f18ec0a20bb5ef55cf93ab46bda678637a2b
jeison1023 <yeisonpuello921@gmail.com>
2025-06-04 22:48:10 -0400

    Ejercicio 2

--- Practica/Ejercicio2/Form1.Designer.cs
+++ Practica/Ejercicio2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ejecicio1
+﻿namespace Ejercicio2
 {
     partial class Form1
     {
@@ -28,26 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(91, 151);
+            this.label1.Location = new System.Drawing.Point(186, 167);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(557, 22);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "\"¡Bienvenido al formulario de Luis alberto y LR-2024-01303!\".";
+            this.label1.Size = new System.Drawing.Size(77, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cambio";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(762, 342);
+            this.ClientSize = new System.Drawing.Size(448, 272);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -57,6 +70,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
     }
 }
